af63a73108ce70c6068eac577d443c440e461520
imsamaritan <aicodemate.talk@gmail.com>
2025-06-01 19:19:28 +0200

    Refactoring order method builder

--- methods/methods.js
+++ methods/methods.js
@@ -2,24 +2,21 @@ function selectAll() {
   this.$method = "selectAll"
 
   this.$select = `SELECT`
-  this.$all = `* FROM ${this.$table}`
+  this.$all = `* FROM ${this.$table} `
   this.$query = [this.$select, this.$all]
 
   return this
 }
 
 function order(details, order = "D") {
-
   if (order === "A") order = "ASC"
   else order = "DESC"
 
-  if (Array.isArray(details.by)) {
-    this.$orderBy = ` ORDER BY ${details.by.join(", ")} ${order}`
-  } else {
-    this.$orderBy = ` ORDER BY ${details.by} ${order}`
-  }
-  this.$query = [...this.$query, this.$orderBy]
+  if (Array.isArray(details.by))
+    this.$orderBy = ` ORDER BY ${details.by.join(", ")} ${order} `
+  else this.$orderBy = ` ORDER BY ${details.by} ${order} `
 
+  this.$query = [...this.$query, this.$orderBy]
   return this
 }
 
