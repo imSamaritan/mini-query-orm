c84ddbf57d0ab2034e054c3b6f9082981de8a912
imsamaritan <aicodemate.talk@gmail.com>
2025-06-07 15:08:32 +0200

    Refactoring mysql connection by using createPool, allowing to set connectionLimit with .env [CONNECTION_LIMIT]

--- db/db.js
+++ db/db.js
@@ -1,15 +1,22 @@
 const mysql = require("mysql2")
 const connectionDebugger = require("debug")("db:connection")
+require("dotenv").config()
+
+const limit = parseInt(process.env.CONNECTION_LIMIT) || 10
 
 const connect = async (settings) => {
-  const db = mysql.createConnection(settings)
+  const poolConfig = {
+    ...settings,
+    waitForConnections: true,
+    connectionLimit: limit,
+  }
 
-  db.connect((error) => {
-    if (error) throw new Error(error)
-    else connectionDebugger(settings)
-  })
+  const pool = mysql.createPool(poolConfig)
 
-  return db.promise()
+  connectionDebugger(`Config : ${JSON.stringify(settings, null, 2)}`)
+  connectionDebugger(`Connection limit : ${limit}`)
+
+  return pool.promise()
 }
 
 module.exports = { connect }
