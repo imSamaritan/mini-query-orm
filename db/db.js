ba78309dce0529e00bf8925f5d3e3b6ac0b96863
imsamaritan <aicodemate.talk@gmail.com>
2025-06-19 19:16:18 +0200

    Added error handling for a pool connection if fails

--- db/db.js
+++ db/db.js
@@ -1,17 +1,22 @@
-const mysql = require("mysql2")
-const connectionDebugger = require("debug")("db:connection")
-require("dotenv").config()
+const mysql = require('mysql2')
+const connectionDebugger = require('debug')('db:connection')
+require('dotenv').config()
 
 const limit = parseInt(process.env.CONNECTION_LIMIT) || 10
 
 const connect = async (settings) => {
+  let pool = null
   const poolConfig = {
     ...settings,
     waitForConnections: true,
     connectionLimit: limit,
   }
 
-  const pool = mysql.createPool(poolConfig)
+  try {
+    pool = mysql.createPool(poolConfig)
+  } catch (error) {
+    throw error
+  }
 
   connectionDebugger(`Config : ${JSON.stringify(settings, null, 2)}`)
   connectionDebugger(`Connection limit : ${limit}`)
